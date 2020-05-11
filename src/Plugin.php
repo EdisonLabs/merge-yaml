66c3142e0b074ff1878660a484654d589328c490
Leandro Nunes <leandro.nunes@pfizer.com>
2020-05-11 18:02:11 -0300

    test abstract

--- src/Plugin.php
+++ src/Plugin.php
@@ -77,10 +77,10 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable
     /**
      * {@inheritdoc}
      */
-    public function deactivate(Composer $composer, IOInterface $io);
+    abstract function deactivate(Composer $composer, IOInterface $io);
 
     /**
      * {@inheritdoc}
      */
-    public function uninstall(Composer $composer, IOInterface $io);
+    abstract function uninstall(Composer $composer, IOInterface $io);
 }
